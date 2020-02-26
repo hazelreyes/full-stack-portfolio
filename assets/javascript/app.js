9927f3ad4873945451aaa1854f6e472345f1bd44
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-02-26 00:27:06 -0800

    made portfolio website

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  function displayModal1() {
+    $("#modal1").modal();
+    $("#modal1").modal("open");
+  }
+  function displayModal2() {
+    $("#modal2").modal();
+    $("#modal2").modal("open");
+  }
+  function displayModal3() {
+    $("#modal3").modal();
+    $("#modal3").modal("open");
+  }
+  function displayModal4() {
+    $("#modal4").modal();
+    $("#modal4").modal("open");
+  }
+  function displayModal5() {
+    $("#modal5").modal();
+    $("#modal5").modal("open");
+  }
+
+  $(document).on("click", ".project1", displayModal1);
+  $(document).on("click", ".project2", displayModal2);
+  $(document).on("click", ".project3", displayModal3);
+  $(document).on("click", ".project4", displayModal4);
+  $(document).on("click", ".project5", displayModal5);
+});
